0af9cd02a0ac32e0205ea90a215dd131f086e26f
svidorkata9-alt <svidorkata9@gmail.com>
2026-09-16 23:39:33 +0300

    Update seal_bot.py

--- seal_bot.py
+++ seal_bot.py
@@ -317,8 +317,15 @@ def cmd_start(message):
     else:
         bot.send_message(user_id, "С возвращением! 🦭 Вы уже зарегистрированы.")
         @bot.message_handler(commands=['stats'])
-       @bot.message_handler(func=lambda message: any(word in message.text.lower() for word in ["похлопай по животику", "шлёпни по пузику", "дай пять животику", "погладь животик", "похлопай по пузику"]))
-     def belly_slap_text(message):
+        @bot.message_handler(func=lambda message: any(word in message.text.lower() for word in [
+    "похлопай по животику",
+    "шлёпни по пузику",
+    "дай пять животику",
+    "погладь животик",
+    "похлопай по пузику"
+]))
+
+def belly_slap_text(message):
     phrases = [
         "🦭 Тюлень радостно хлопает себя по животику! Плюх-плюх! 😄",
         "🦭 *шлёп-шлёп* Так приятно! Ещё? 😊",
